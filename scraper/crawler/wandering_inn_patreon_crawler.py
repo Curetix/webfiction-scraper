a8152180effb401f07d3edbd4c75a21b2a1afca1
CURETIX <curetix@protonmail.com>
2020-09-18 17:16:53 +0200

    Add ability to extract password from Patreon post stream

--- scraper/crawler/wandering_inn_patreon_crawler.py
+++ scraper/crawler/wandering_inn_patreon_crawler.py
@@ -9,10 +9,65 @@ from ..exception import ElementNotFoundException
 
 # noinspection DuplicatedCode
 class WanderingInnPatreonCrawler(Crawler):
-    def __init__(self, config: Box):
+    def __init__(self, config: Box, patreon_cookie=None):
         super().__init__(config)
-        self.session = Session()
         echo("Using WanderingInnPatreonCrawler")
+
+        self.session = Session()
+
+        self.attempted_patreon_password = False
+
+        if patreon_cookie:
+            self.patreon_session = Session()
+            self.cookies = {"session_id": patreon_cookie}
+        else:
+            self.patreon_session = None
+
+    def get_patreon_posts(self):
+        if not self.patreon_session:
+            echo("Patreon session not initialized! Please provide the cookie!")
+            return
+
+        payload = {
+            "fields[post]": "content,published_at,patreon_url,post_type,title,url",
+            "filter[campaign_id]": 568211,
+            "sort": "-published_at"
+        }
+
+        r = self.patreon_session.get("https://www.patreon.com/api/posts", params=payload, cookies=self.cookies)
+
+        if r:
+            return r.json().get("data")
+
+    def get_patreon_password(self, chapter_title, chapter_url):
+        echo("Trying to automatically find password on Patreon...")
+
+        posts = self.get_patreon_posts()
+
+        if not posts:
+            return
+
+        found_post = None
+
+        post_by_title = list(filter(lambda p: p["attributes"].get("title", "").strip() == chapter_title, posts))
+
+        if len(post_by_title) > 0:
+            found_post = post_by_title[0]
+        else:
+            post_by_url = list(filter(lambda p: p["attributes"].get("content").contains(chapter_url.replace("https://", "")), posts))
+
+            if len(post_by_url) > 0:
+                found_post = post_by_url[0]
+
+        if not found_post:
+            return
+
+        soup = BeautifulSoup(found_post["attributes"]["content"], "lxml")
+        password_element = soup.select_one("p:contains(\"Password:\")")
+
+        if password_element:
+            echo("Found a password! But does it work?")
+            return password_element.find_next_sibling().get_text().strip()
 
     def download_chapter(self, url):
         r = self.session.get(url)
@@ -30,18 +85,25 @@ class WanderingInnPatreonCrawler(Crawler):
             .replace("Protected: ", "")
 
         if soup.select_one(".post-password-form"):
-            if confirm("Protected chapter %s reached! Do you have the password?" % title):
-                password = prompt("Enter the password")
+            password = None
 
-                self.session.post("https://wanderinginn.com/wp-pass.php", data={
-                    'post_password': password,
-                    'Submit': 'Enter'
-                }, headers={
-                    'referer': r.url
-                })
+            if self.patreon_session and not self.attempted_patreon_password:
+                self.attempted_patreon_password = True
+                password = self.get_patreon_password(title, url)
 
-                return self.download_chapter(r.url)
-            else:
-                return r.url, None, None, None
+            if not password:
+                if confirm("Protected chapter %s reached! Do you have the password?" % title):
+                    password = prompt("Enter the password")
+                else:
+                    return r.url, None, None, None
+
+            self.session.post("https://wanderinginn.com/wp-pass.php", data={
+                'post_password': password,
+                'Submit': 'Enter'
+            }, headers={
+                'referer': r.url
+            })
+
+            return self.download_chapter(r.url)
 
         return r.url, title, r.content, soup
