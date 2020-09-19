5006c46f3cfcf822f25807d89cec17d1fc174569
CURETIX <curetix@protonmail.com>
2020-09-19 18:07:10 +0200

    Actually fix setup.py?

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="WebFictionScraper",
     version="1.0",
-    packages=find_packages(),
+    py_modules=["client", "scraper"],
     package_data={"scraper": ["configs/*.yaml"]},
     install_requires=[
         "ebooklib",
