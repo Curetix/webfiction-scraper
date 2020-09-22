bca722a2f2eb73446e7fcf59d4b549dfce5dd232
Curetix <curetix@protonmail.com>
2020-09-22 10:02:23 +0200

    Remove old chapter fixes method

--- scraper/converter.py
+++ scraper/converter.py
@@ -4,7 +4,6 @@ import sys
 from multiprocessing import Pool, cpu_count
 
 from bs4 import BeautifulSoup
-from bs4.element import Tag
 from click import echo
 
 from .manifest import Manifest
@@ -98,38 +97,6 @@ class Converter:
         echo("Converted chapter: %s" % title)
 
         return doc
-
-    @staticmethod
-    def apply_chapter_fix(chapter, soup, content_el):
-        def fix_nothing():
-            pass
-
-        def fix_metaworld_chronicles_paragraphs():
-            """Some chapters have <div> elements instead of proper <p> paragraphs, so rename all <div> tags to <p>"""
-            for el in content_el.find_all("div", recursive=False):
-                el.name = "p"
-
-        def fix_metaworld_chronicles_single_div():
-            div_el = content_el.select_one("div")
-            for e in div_el.contents:
-                if type(e) is Tag and e.name == "br":
-                    continue
-                new_p = soup.new_tag("p")
-                content_el.append(new_p)
-                content_el.append("\n")
-                content_el.select_one("p:last-of-type").append(e)
-            div_el.decompose()
-
-        fixes = {
-            "https://www.royalroad.com/fiction/14167/metaworld-chronicles/chapter/494392/chapter-346-bride-and-groom": fix_metaworld_chronicles_paragraphs,
-            "https://www.royalroad.com/fiction/14167/metaworld-chronicles/chapter/541710/chapter-368-even-death-may-die": fix_metaworld_chronicles_paragraphs,
-            "https://www.royalroad.com/fiction/14167/metaworld-chronicles/chapter/551476/chapter-372-a-little-knowledge": fix_metaworld_chronicles_paragraphs,
-            "https://www.royalroad.com/fiction/14167/metaworld-chronicles/chapter/553925/chapter-373-the-burden-of-knowledge": fix_metaworld_chronicles_single_div,
-        }
-
-        fixes.get(chapter.get("url"), fix_nothing)()
-
-        return content_el
 
     def convert_file(self, index, chapter):
         in_file = os.path.join(self.files.cache_folder, chapter.get("file"))
