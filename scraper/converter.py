88bc8990ebe82005c67daf74f5f249fcbcc46e66
Curetix <me@curetix.eu>
2022-04-08 09:59:42 +0200

    Option to skip most conversion steps

--- scraper/converter.py
+++ scraper/converter.py
@@ -18,6 +18,8 @@ class Converter:
         self.selectors = config.selectors
         self.substitutions = config.substitutions
         self.remove_empty_elements = config.remove_empty_elements
+        self.skip_chapters = config.skip_chapters
+        self.skip_conversion = config.skip_conversion
         self.manifest = Manifest(config.files.manifest_file)
 
     def convert(self, doc, chapter):
@@ -29,57 +31,58 @@ class Converter:
         if not content_el:
             raise ElementNotFoundException("Content element not found for chapter: " + title)
 
-        self.apply_chapter_fix(chapter, soup, content_el, self.selectors.content_element)
+        if not self.skip_conversion:
+            self.apply_chapter_fix(chapter, soup, content_el, self.selectors.content_element)
 
-        last_p_el = content_el.select_one(self.selectors.content_element + " > p:last-of-type")
-
-        if not last_p_el:
-            # In some cases (on Royal Road), the content is not directly inside the content_el, but wrapped in a single
-            # div inside the content_el, so if a div exists we unwrap it and see what happens
-            if "royalroad.com" in chapter.get("url", ""):
-                unwrap_toplevel_divs(soup, content_el, self.selectors.content_element)
-                last_p_el = content_el.select_one(self.selectors.content_element + " > p:last-of-type")
+            last_p_el = content_el.select_one(self.selectors.content_element + " > p:last-of-type")
 
             if not last_p_el:
-                raise ElementNotFoundException("Last paragraph not found for chapter: " + title)
+                # In some cases (on Royal Road), the content is not directly inside the content_el, but wrapped in a single
+                # div inside the content_el, so if a div exists we unwrap it and see what happens
+                if "royalroad.com" in chapter.get("url", ""):
+                    unwrap_toplevel_divs(soup, content_el, self.selectors.content_element)
+                    last_p_el = content_el.select_one(self.selectors.content_element + " > p:last-of-type")
 
-        # If a cut-off element is specified, set its previous sibling as last element, otherwise the last paragraph
-        if s := self.selectors.get("cut_off_element"):
-            if type(s) == str:
-                s = [s]
+                if not last_p_el:
+                    raise ElementNotFoundException("Last paragraph not found for chapter: " + title)
 
-            for e in s:
-                last_p_el = content_el.select_one(e)
-                if last_p_el:
-                    last_p_el = last_p_el.find_previous_sibling()
-                    break
+            # If a cut-off element is specified, set its previous sibling as last element, otherwise the last paragraph
+            if s := self.selectors.get("cut_off_element"):
+                if type(s) == str:
+                    s = [s]
 
-        # Delete everything after the last element
-        while last_p_el and last_p_el.find_next_sibling():
-            last_p_el.find_next_sibling().decompose()
+                for e in s:
+                    last_p_el = content_el.select_one(e)
+                    if last_p_el:
+                        last_p_el = last_p_el.find_previous_sibling()
+                        break
 
-        # Remove empty elements if enabled
-        if self.remove_empty_elements:
-            for el in content_el.find_all(recursive=False):
-                if el.name != "hr" and el.get_text().strip() == "":
-                    el.decompose()
+            # Delete everything after the last element
+            while last_p_el and last_p_el.find_next_sibling():
+                last_p_el.find_next_sibling().decompose()
 
-        # Apply substitutions with CSS selector
-        for s in [s for s in self.substitutions if s.selector_type == "css"]:
-            if s.chapter_url and s.chapter_url != chapter.get("url"):
-                continue
+            # Remove empty elements if enabled
+            if self.remove_empty_elements:
+                for el in content_el.find_all(recursive=False):
+                    if el.name != "hr" and el.get_text().strip() == "":
+                        el.decompose()
 
-            els = content_el.select(s.selector)
+            # Apply substitutions with CSS selector
+            for s in [s for s in self.substitutions if s.selector_type == "css"]:
+                if s.chapter_url and s.chapter_url != chapter.get("url"):
+                    continue
 
-            if not len(els):
-                echo("Chapter '%s': No matches for selector '%s'. Please check if the config is up-to-date." % (title, s.selector))
-                continue
+                els = content_el.select(s.selector)
 
-            for el in els:
-                if s.replace_with:
-                    el.replace_with(s.replace_with)
-                else:
-                    el.decompose()
+                if not len(els):
+                    echo("Chapter '%s': No matches for selector '%s'. Please check if the config is up-to-date." % (title, s.selector))
+                    continue
+
+                for el in els:
+                    if s.replace_with:
+                        el.replace_with(s.replace_with)
+                    else:
+                        el.decompose()
 
         # Change the content elements tag to <body> and add the chapter title
         content_el.name = "body"
@@ -127,7 +130,7 @@ class Converter:
 
     def convert_all(self):
         with Pool(processes=cpu_count()) as pool:
-            chapters_to_convert = filter(lambda t: not t[1].get("converted"), enumerate(self.manifest))
+            chapters_to_convert = filter(lambda t: not t[1].get("converted") and not t[1].get("url") in self.skip_chapters, enumerate(self.manifest))
             results = [pool.apply_async(self.convert_file, args=m) for m in chapters_to_convert]
             converted_chapters = [p.get() for p in results]
 
