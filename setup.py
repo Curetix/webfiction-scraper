ce8b64523a24b50cf7be408237950970e55cc7d0
Curetix <me@curetix.eu>
2024-03-15 15:55:25 +0100

    Fix Poetry error `webserial_scraper does not contain any element`

--- setup.py
+++ setup.py
@@ -2,25 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name="WebFictionScraper",
-    version="1.4",
+    version="1.4.0",
     author="Curetix",
-    author_email="contact@curetix.eu",
+    author_email="me@curetix.eu",
     url="https://gitlab.com/Curetix/python-webfiction-scraper",
     description="Scrape web fictions into eBooks.",
     packages=find_packages(),
     py_modules=["webfictionscraper"],
-    package_data={"scraper": ["configs/*.yaml"]},
     install_requires=[
-        "ebooklib",
-        "pyyaml",
-        "click",
-        "schema",
-        "requests",
         "beautifulsoup4",
-        "lxml",
-        "python-box[all]>=5.0,<8.0",
-        "pyinquirer",
-        "appdirs"
+        "click",
+        "platformdirs",
+        "ebooklib",
+        "python-box[all]",
+        "questionary",
+        "requests",
+        "schema"
     ],
     entry_points="""
         [console_scripts]
